c25d32aab8d19f9fffa1c12a60efb7fbd6269221
glitzereinhorn270487 <glitzereinhorn270487@gmail.com>
2025-10-07 16:41:55 +0000

    ADD REDIS

--- config/settings.py
+++ config/settings.py
@@ -8,6 +8,11 @@ class Settings:
     QUICKNODE_RPC_URL: str = os.getenv("QUICKNODE_RPC_URL", "")
     QUICKNODE_WSS_URL: str = os.getenv("QUICKNODE_WSS_URL", "")
 
+     # Upstash Redis
+    UPSTASH_REDIS_URL: str = os.getenv("UPSTASH_REDIS_URL", "")
+    UPSTASH_REDIS_TOKEN: str = os.getenv("UPSTASH_REDIS_TOKEN", "")
+
+
     # Telegram API Keys
     TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
     TELEGRAM_CHAT_ID: str = os.getenv("TELEGRAM_CHAT_ID", "")
@@ -26,7 +31,7 @@ class Settings:
     GOPLUS_API_URL: str = "https://api.gopluslabs.io/api/v1/token_security/1" # 1 = Solana Chain ID
 
 
-    if not all([QUICKNODE_RPC_URL, QUICKNODE_WSS_URL, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, GOOGLE_CLOUD_PROJECT]):
+    if not all([QUICKNODE_RPC_URL, QUICKNODE_WSS_URL, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, GOOGLE_CLOUD_PROJECT, UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN]):
         raise ValueError("Wichtige API-Schlüssel oder Konfigurationen fehlen in den Umgebungsvariablen.")
 
 settings = Settings()
